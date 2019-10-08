a727964c9a35e0daeb25c87c8d030448dff7896c
Mohammad Sakr <mohamed.sakr@digis2.com>
2019-10-08 14:39:40 +0200

    Initial Commit

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-import { GraphQlServer } from 'graphql-yoga';
+import { GraphQLServer } from 'graphql-yoga';
 
 // Type definitions
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+  }
+`;
 
 // Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'this is my first query';
+    },
+    name() {
+      return 'My name is mohammad';
+    }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => {
+  console.log('Server is running');
+});
