0796cf5acc9c71123793686a7e3e2da71e433797
Mohammad Sakr <mohamed.sakr@digis2.com>
2019-10-16 01:17:51 +0200

    Lesson 2

--- app.js
+++ app.js
@@ -3,19 +3,35 @@ import { GraphQLServer } from 'graphql-yoga';
 // Type definitions
 const typeDefs = `
   type Query {
-    hello: String!
+    greeting(name: String, position: String): String!
+    me: User!
+  }
+
+  type User {
+    id: ID!
     name: String!
+    mail: String
+    age: Int!
   }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return 'this is my first query';
+    greeting(parent, args, ctx, info) {
+      if (args.name && args.position) {
+        return `Hello! ${args.name}, you are ${args.position}`;
+      }
+      console.log(args);
+      return 'Hello!';
     },
-    name() {
-      return 'My name is mohammad';
+    me() {
+      return {
+        id: '123asd',
+        name: 'mohammad',
+        mail: 'mail@yahoo.com',
+        age: 12
+      };
     }
   }
 };
