eb675aea17c46e256556798b72da8291f5dcea95
Mohammad Sakr <mohamed.sakr@digis2.com>
2019-10-20 16:23:02 +0200

    lesson 13

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ import { GraphQLServer } from 'graphql-yoga';
 // Type definitions
 const typeDefs = `
   type Query {
+    users: [User!]!
     greeting(name: String, position: String): String!
+    add(numbers: [Float!]!): Float!
     me: User!
+    grades: [Int!]!
   }
 
   type User {
@@ -18,6 +21,20 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
+    users(parents, args, ctx, info) {
+      return [
+        { id: '12qw', name: 'Mohammad', mail: 'mail@yahoo.com', age: 12 },
+        { id: '12qwwe', name: 'Mohammad 1', mail: 'mail1@yahoo.com', age: 13 }
+      ];
+    },
+    add(parents, args, ctx, info) {
+      if (args.numbers.length === 0) {
+        return 0;
+      }
+      return args.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      }, 0);
+    },
     greeting(parent, args, ctx, info) {
       if (args.name && args.position) {
         return `Hello! ${args.name}, you are ${args.position}`;
@@ -32,6 +49,9 @@ const resolvers = {
         mail: 'mail@yahoo.com',
         age: 12
       };
+    },
+    grades(parent, args, ctx, info) {
+      return [12, 15, 23];
     }
   }
 };
