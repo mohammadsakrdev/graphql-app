a1ec111ae06774abe15d5a01e25040c2260c4d03
Mohammad Sakr <mohamed.sakr@digis2.com>
2019-10-30 11:50:04 +0200

    Begin Subscription

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 import db from './src/db';
 import Query from './src/resolvers/Query';
 import Mutation from './src/resolvers/Mutation';
+import Subscription from './src/resolvers/Subscription';
 import User from './src/resolvers/User';
 import Post from './src/resolvers/Post';
 import Comment from './src/resolvers/Comment';
 
+const pubSub = new PubSub();
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers: { Query, Mutation, User, Post, Comment },
-  context: { db }
+  resolvers: { Query, Mutation, Subscription, User, Post, Comment },
+  context: { db, pubSub }
 });
 
 server.start(() => {
